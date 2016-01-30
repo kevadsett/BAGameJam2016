928971aa5d2bf5a0a2e13bf8defe24b1ae0b022b
Kev Adsett <kev@bossalien.com>
2016-01-30 16:20:00 +0000

    Make buttons to select demons by class.

---
+++ Unity/Assets/Code/Companion/BookButtons.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BookButtons : MonoBehaviour {
+
+	public void OnAirClick()
+	{
+		CompanionDetailState.DemonType = eDemonType.Air;
+		StateMachine.SetState( eState.CompanionDetail );
+	}
+
+	public void OnEarthClick()
+	{
+		CompanionDetailState.DemonType = eDemonType.Earth;
+		StateMachine.SetState( eState.CompanionDetail );
+	}
+
+	public void OnFireClick()
+	{
+		CompanionDetailState.DemonType = eDemonType.Fire;
+		StateMachine.SetState( eState.CompanionDetail );
+	}
+
+	public void OnWaterClick()
+	{
+		CompanionDetailState.DemonType = eDemonType.Water;
+		StateMachine.SetState( eState.CompanionDetail );
+	}
+}
