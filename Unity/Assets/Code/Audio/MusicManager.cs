d7619ae803e1dcb0838c5789b02ed4c3bd721176
Richard Bangs <bangs@bossalien.com>
2016-01-30 22:37:15 +0000

    - Music Manager now supports Angels vs Demons.

--- Unity/Assets/Code/Audio/MusicManager.cs
+++ Unity/Assets/Code/Audio/MusicManager.cs
@@ -4,21 +4,37 @@ using System.Collections.Generic;
 
 public class MusicManager : MonoBehaviour
 {
-	public List<AudioClip> Layers = new List<AudioClip>();
+	public List<AudioClip> AngelLayers = new List<AudioClip>();
+	public List<AudioClip> DemonLayers = new List<AudioClip>();
+
+	public List<AudioSource> AngelSources = new List<AudioSource>();
+	public List<AudioSource> DemonSources = new List<AudioSource>();
 
 	void Start()
-	{		
-		foreach( var layer in Layers )
+	{
+		foreach( var layer in AngelLayers )
 		{
-			var newGO = new GameObject( layer.name );
-			newGO.transform.parent = transform;
-
-			var audioSource = newGO.AddComponent< AudioSource >();
-			audioSource.loop = true;
-			audioSource.clip = layer;
-
-			audioSource.Play();
+			AngelSources.Add( CreateSource( layer ) );
 		}
+
+		foreach( var layer in DemonLayers )
+		{
+			DemonSources.Add( CreateSource( layer ) );
+		}
+	}
+
+	AudioSource CreateSource( AudioClip audioClip )
+	{
+		var newGO = new GameObject( audioClip.name );
+		newGO.transform.parent = transform;
+
+		var audioSource = newGO.AddComponent< AudioSource >();
+		audioSource.loop = true;
+		audioSource.clip = audioClip;
+
+		//audioSource.Play();
+
+		return audioSource;
 	}
 }
 
