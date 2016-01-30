45395282ee16cf05d0f7b0e2064205a49705cb9c
Richard Bangs <bangs@bossalien.com>
2016-01-30 22:09:30 +0000

    - Added Music Manager.

---
+++ Unity/Assets/Code/Audio/MusicManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class MusicManager : MonoBehaviour
+{
+	public List<AudioClip> Layers = new List<AudioClip>();
+
+	void Start()
+	{		
+		foreach( var layer in Layers )
+		{
+			var newGO = new GameObject( layer.name );
+			newGO.transform.parent = transform;
+
+			var audioSource = newGO.AddComponent< AudioSource >();
+			audioSource.loop = true;
+			audioSource.clip = layer;
+
+			audioSource.Play();
+		}
+	}
+}
+
