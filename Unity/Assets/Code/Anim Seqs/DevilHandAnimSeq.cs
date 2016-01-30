bedb255bdab6b1fa847baea5d2e48151716b87ab
Gavin Jones <notoriousknave@gmail.com>
2016-01-30 12:55:38 +0000

    Refactored animation sequences into their own classes, and made them ScriptableObjects so we can tweak them!

---
+++ Unity/Assets/Code/Anim Seqs/DevilHandAnimSeq.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System;
+
+[CreateAssetMenu]
+public class DevilHandAnimSeq : ScriptableObject
+{
+	[SerializeField] private SimpleTween TweenDevilHandOn;
+	[SerializeField] private SimpleTween TweenDevilHandClose;
+	[SerializeField] private SimpleTween TweenDevilHandOff;
+	[SerializeField] private Vector3 HandSpacing;
+	[SerializeField] private Vector3 OffscreenPos;
+
+	public void Trigger(AnimateToPoint devilHandAnimator1,
+						AnimateToPoint devilHandAnimator2,
+						AnimateToPoint victimAnimator,
+						Vector3 podiumPos,
+						Action onComplete) {
+
+		devilHandAnimator1.gameObject.SetActive(true);
+		devilHandAnimator2.gameObject.SetActive(true);
+		devilHandAnimator1.transform.position = OffscreenPos - HandSpacing;
+		devilHandAnimator2.transform.position = OffscreenPos + HandSpacing;
+		victimAnimator.transform.position = podiumPos;
+
+		devilHandAnimator1.Trigger(TweenDevilHandOn, podiumPos - HandSpacing);
+		devilHandAnimator2.Trigger(TweenDevilHandOn, podiumPos + HandSpacing, () => {
+			devilHandAnimator1.Trigger(TweenDevilHandClose, podiumPos);
+			devilHandAnimator2.Trigger(TweenDevilHandClose, podiumPos, () => {
+				victimAnimator.Trigger(TweenDevilHandOff, OffscreenPos);
+				devilHandAnimator1.Trigger(TweenDevilHandOff, OffscreenPos);
+				devilHandAnimator2.Trigger(TweenDevilHandOff, OffscreenPos, () => {
+					devilHandAnimator1.gameObject.SetActive(false);
+					devilHandAnimator2.gameObject.SetActive(false);
+					victimAnimator.gameObject.SetActive(false);
+					onComplete();
+				});
+			});
+		});
+	}
+}
--- Unity/Assets/Code/AnimationController.cs
+++ Unity/Assets/Code/AnimationController.cs
@@ -6,13 +6,10 @@ public class AnimationController : MonoBehaviour {
 	[SerializeField] private AnimateToPoint devilHandAnimator1;
 	[SerializeField] private AnimateToPoint devilHandAnimator2;
 	[SerializeField] private AnimateToPoint victimAnimator;
-	[SerializeField] private SimpleTween TweenToPodium;
-	[SerializeField] private SimpleTween TweenGodHandOn;
-	[SerializeField] private SimpleTween TweenToChoir;
-	[SerializeField] private SimpleTween TweenGodHandOff;
-	[SerializeField] private SimpleTween TweenDevilHandOn;
-	[SerializeField] private SimpleTween TweenDevilHandClose;
-	[SerializeField] private SimpleTween TweenDevilHandOff;
+
+	[SerializeField] private PodiumAppearAnimSeq podiumAnims;
+	[SerializeField] private DevilHandAnimSeq devilHandAnims;
+	[SerializeField] private GodHandAnimSeq godHandAnims;
 
 	private enum State { Queue, Podium, Choir, Hell, Animating };
 	private State MyState;
@@ -22,50 +19,34 @@ public class AnimationController : MonoBehaviour {
 	private Vector3 choirPos = Vector3.right * 5.0f;
 	private Vector3 godHandPos = Vector3.up * 5.0f;
 	private Vector3 devilHandPos = Vector3.down * 5.0f;
-	private Vector3 devilHandOffset = Vector3.right * 2.0f;
 
 	public void Initialize() {
 		MyState = State.Queue;
-		godHandAnimator.transform.position = godHandPos;
 		victimAnimator.transform.position = queuePos;
-		devilHandAnimator1.transform.position = devilHandPos - devilHandOffset;
-		devilHandAnimator2.transform.position = devilHandPos + devilHandOffset;
+		victimAnimator.gameObject.SetActive(true);
+		godHandAnimator.gameObject.SetActive(false);
+		devilHandAnimator1.gameObject.SetActive(false);
+		devilHandAnimator2.gameObject.SetActive(false);
 	}
 
 	public void QueueToPodium() {
-		Debug.Log("QueueToPodium()");
 		MyState = State.Animating;
-		victimAnimator.Trigger(TweenToPodium, podiumPos, () => {
+		podiumAnims.Trigger(victimAnimator, queuePos, podiumPos, () => {
 			MyState = State.Podium;
 		});
 	}
 
 	public void PodiumToChoir() {
-		Debug.Log("PodiumToChoir()");
 		MyState = State.Animating;
-		godHandAnimator.Trigger(TweenGodHandOn, podiumPos, () => {
-			victimAnimator.Trigger(TweenToChoir, choirPos);
-			godHandAnimator.Trigger(TweenToChoir, choirPos, () => {
-				godHandAnimator.Trigger(TweenGodHandOff, godHandPos, () => {
-					MyState = State.Choir;
-				});
-			});
+		godHandAnims.Trigger(godHandAnimator, victimAnimator, podiumPos, choirPos, () => {
+			MyState = State.Choir;
 		});
 	}
 
 	public void PodiumToHell() {
-		Debug.Log("PodiumToHell()");
 		MyState = State.Animating;
-		devilHandAnimator1.Trigger(TweenDevilHandOn, podiumPos - devilHandOffset);
-		devilHandAnimator2.Trigger(TweenDevilHandOn, podiumPos + devilHandOffset, () => {
-			devilHandAnimator1.Trigger(TweenDevilHandClose, podiumPos);
-			devilHandAnimator2.Trigger(TweenDevilHandClose, podiumPos, () => {
-				victimAnimator.Trigger(TweenDevilHandOff, devilHandPos);
-				devilHandAnimator1.Trigger(TweenDevilHandOff, devilHandPos);
-				devilHandAnimator2.Trigger(TweenDevilHandOff, devilHandPos, () => {
-					MyState = State.Hell;
-				});
-			});
+		devilHandAnims.Trigger(devilHandAnimator1, devilHandAnimator2, victimAnimator, podiumPos, () => {
+			MyState = State.Hell;
 		});
 	}
 
